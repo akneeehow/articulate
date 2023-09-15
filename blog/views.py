34b84760492d42286fb776228ae902c0ebf35cee
Nehal <nehalc2107@gmail.com>
2023-09-15 17:09:36 +0100

    “lol”

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("blog:post_list")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="blog/register.html", context={"register_form":form})
